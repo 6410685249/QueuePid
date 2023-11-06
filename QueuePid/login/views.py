2e8004d2bf1ec9b0c88a602e09c4df1795b59db3
Peekkan Piankan <peekkan@cn331>
2023-11-07 01:23:04 +0700

    add login

--- QueuePid/login/views.py
+++ QueuePid/login/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect,HttpResponse
+from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("home"))
+        else:
+            return render(request, 'login.html', {
+                'message': 'Invalid credentials!'
+            })
+    return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'login.html', {
+        'message': 'Logged out'
+    })
+
+def home(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    
+    return render(request, 'home.html')
