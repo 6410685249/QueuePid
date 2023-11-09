546c7eef0405d977f5f69fdecd6984461dc94ff9
Chalisa Thummeraj <6410685041@cn103>
2023-11-09 19:04:57 +0700

    Add about page

--- QueuePid/about/urls.py
+++ QueuePid/about/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.queuepid,name='queuepid')
+    path('',views.queuepid,name='queuepid'),
+    path('about/',views.about_queuepid,name='about_queuepid'),
+    path('more/',views.more_about_us, name="more_about_us"),
 ]
