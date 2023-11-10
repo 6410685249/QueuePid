c11d4d8290066ab0eccc228df358be8acf1418de
Peerapat Ngamgsanga <6410685223@cn331>
2023-11-10 19:21:04 +0700

    fix

--- QueuePid/customers/urls.py
+++ QueuePid/customers/urls.py
@@ -2,31 +2,20 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('restaurant_list',views.list_restaurant,name='restaurant_list'),
-    path('restaurant_list/history',views.history,name='go_to_history_page'),
-    
-    path('restaurant_list/history_back_to_home',views.in_history_back_to_home,name='history_back_to_home'),
-    path('restaurant_list/about',views.go_to_about_page,name='go_to_about_page'),
-    path('restaurant_list/go_to_about_page',views.about,name='about'),
-    path('restaurant_list/in_about_back_to_home',views.in_about_back_to_home,name='in_about_back_to_home'),
+    path('customer_home',views.list_restaurant,name='restaurant_list'),
+ 
+    path('customer_home/about',views.go_to_about_page,name='about_page'),
+    path('customer_home/go_to_about_page',views.about,name='about'),
+    path('customer_home/acount_back',views.acount_back,name='acount_back'),
 
-    path('restaurant_list/wallet',views.go_to_wallet_page,name='go_to_wallet_page'),
-    path('restaurant_list/go_to_wallet_page',views.wallet,name='wallet'),
-    path('restaurant_list/in_wallet_back_to_home',views.in_wallet_back_to_home,name='in_wallet_back_to_home'),
+    path('customer_home/wallet',views.wallet_page,name='wallet_page'),
+    path('customer_home/account',views.account_page,name='account_page'),
 
-    path('restaurant_list/account',views.go_to_account_page,name='go_to_account_page'),
-    path('restaurant_list/go_to_wallet_page',views.account,name='account'),
-    path('restaurant_list/in_account_back_to_home',views.in_account_back_to_home,name='in_account_back_to_home'),
-    path('restaurant_list/account',views.go_to_account_page,name='go_to_account_page'),
+    path('customer_home/account/edit_page',views.edit_page,name='edit_page'),
+    path('customer_home/account/success_edit',views.success_edit,name='success_edit'),
+    path('customer_home/account/change_password',views.change_password,name='change_password'),
+    path('customer_home/account/success_password',views.success_password,name='success_password'),
 
-    path('restaurant_list/account/edit_profile',views.go_to_edit_profile_page,name='go_to_edit_profile_page'),
-    path('restaurant_list/account/in_edit_back_account',views.in_edit_back_account,name='in_edit_back_account'),
-    path('restaurant_list/account/in_success_edit_back_account',views.in_success_edit_back_account,name='in_success_edit_back_account'),
-
-    path('restaurant_list/account/go_to_change_password_page',views.go_to_change_password_page,name='go_to_change_password_page'),
-    path('restaurant_list/account/in_change_password_back_to_account',views.in_change_password_back_to_account,name='in_change_password_back_to_account'),
-    path('restaurant_list/account/change_password',views.change_password,name='change_password'),
-    path('restaurant_list/account/in_complete_change_password_back_to_account',views.complete_password,name='in_complete_change_password_back_to_account'),
-
+    path('customer_home/history',views.history,name='history'),
 
 ]
--- QueuePid/customers/views.py
+++ QueuePid/customers/views.py
@@ -8,66 +8,31 @@ from django.shortcuts import render, redirect
 from .models import Restaurant,Historically
 from login.models import User_info
 from login.views import logout_view
+
 def list_restaurant(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
-    restaurant_list = Restaurant.objects.all()
-    name_and_location = [(i.name,i.location) for i in Restaurant.objects.all()]
-    return render(request, 'customer_home.html', {'form': name_and_location})
+    return render(request, 'customer_home.html', {'form': [(i.name,i.location) for i in Restaurant.objects.all()]})
 
-def history(request):
-    print('IN history fuinc')
-    if request.method == 'POST':
-        print('IN if history fuinc')
-        return history_list(request)
-    return list_restaurant(request)
-
-def history_list(request): # render to html
-    print('IN if history_list fuinc')
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    return render(request, 'customer_history.html', {'history': Historically.objects.all()})
-
-def in_history_back_to_home(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    if request.method == 'POST':
-        return list_restaurant(request)
-    return history_list(request)
 
 def go_to_about_page(request):
-
-    if request.method == 'POST':
-        return about(request)
-    return list_restaurant(request)
+    return about(request)
 
 def about(request): # render to html
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     return render(request,'customer_about.html')
 
-def in_about_back_to_home(request):
+def acount_back(request):
 
-    if request.method == 'POST':
-        return list_restaurant(request)
-    return about(request)
+    return list_restaurant(request)
 
 def wallet(request): # render to html
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     return render(request,'customer_wallet.html')
 
-def go_to_wallet_page(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    if request.method == 'POST':
-        return wallet(request)
-    return list_restaurant(request)
-
-def in_wallet_back_to_home(request):
-
-    if request.method == 'POST':
-        return list_restaurant(request)
+def wallet_page(request):
     return wallet(request)
 
 def account(request): # render to html
@@ -76,45 +41,18 @@ def account(request): # render to html
     user = User_info.objects.get(username=request.user)
     
     return render(request,'customer_account.html',{'user':user})
-
-def go_to_account_page(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    if request.method == 'POST':
-        return account(request)
-    return list_restaurant(request)
-
-def in_account_back_to_home(request):
-
-    if request.method == 'POST':
-        return list_restaurant(request)
+def account_page(request):
     return account(request)
 
-def edit_profile(request):
-    if not request.user.is_authenticated:
-        return HttpResponseRedirect(reverse('login'))
-    from django.shortcuts import render, redirect
 
-
-def edit_profile(request):
+def edit_page(request):
     # Get the current user's profile (replace with your logic to get the user's profile)
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
-    user_profile = User_info.objects.get(username=request.user)  # Assuming you have a foreign key relationship to User
+    user_profile = User_info.objects.get(username=request.user)  
     return render(request,'customer_edit_profile.html',{'user_profile': user_profile})
 
-
-def go_to_edit_profile_page(request):
-    if request.method == 'POST':
-        return edit_profile(request)
-    return account(request)
-
-def in_edit_back_account(request):
-    if request.method == 'POST':
-        return account(request)
-    return edit_profile(request)
-
-def in_success_edit_back_account(request):
+def success_edit(request):
     if request.method == 'POST':
 
 
@@ -129,25 +67,15 @@ def in_success_edit_back_account(request):
 
         user_info.save()
         return account(request)
-    return edit_profile(request)
-
-def go_to_change_password_page(request):
-    if request.method == 'POST':
-        return change_password(request)
-    return account(request)
+    return edit_page(request)
 
 def change_password(request):
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
     return render(request,'customer_change_password.html')
 
-def in_change_password_back_to_account(request):
-    
-    if request.method == 'POST':
-        return account(request)
-    return change_password(request)
 
-def complete_password(request):
+def success_password(request):
     if request.method == 'POST':
         new_password = request.POST['password']
         user = request.user
@@ -156,3 +84,9 @@ def complete_password(request):
 
         return logout_view(request)
     return change_password(request)
+
+
+def history(request): # render to html
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request,'customer_history.html',{'history': [i for i in Historically.objects.filter(username=request.user)]})
