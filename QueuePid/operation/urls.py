7d8c950c429e8394c8b9e14661c5a2ee59b7be12
Peerapat Ngamgsanga <6410685223@cn331>
2023-11-17 22:15:54 +0700

    get pull

--- QueuePid/operation/urls.py
+++ QueuePid/operation/urls.py
@@ -4,6 +4,11 @@ urlpatterns = [
     path('customer_home/booking',views.booking,name='booking'),
     path('customer_home/get_number_of_customer',views.get_number_of_customer,name='get_number_of_customer'),
     path('customer_home/customer_status',views.customer_status,name='customer_status'),
+    path('customer_home/customer_payment',views.customer_payment,name='customer_payment'),
+    path('customer_complete',views.customer_complete,name='customer_complete'),
+    path('customer_review',views.customer_review,name='customer_review'),
+    path('customer_report',views.customer_report,name='customer_report'),
 
-    
+
+
 ]
--- QueuePid/operation/views.py
+++ QueuePid/operation/views.py
@@ -9,6 +9,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from .models import Operation
 from django.utils import timezone
+from queueman.models import *
 total_seconds = 0
 
 def booking(request, restaurant_name):
@@ -41,3 +42,41 @@ def get_number_of_customer(request):
         user.save()
 
         return render(request,'customer_status.html')
+
+def customer_payment(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login')) 
+    operation_user = Operation.objects.get(customer_username=request.user.username)
+    user = User_info.objects.get(username = request.user)
+    queueman = User.objects.get(username=operation_user.queueMan_username)
+    user_queueman = Queueman.objects.get(username=queueman)
+
+    time_end = timezone.now()
+    time_start = operation_user.date
+    time_diff = (time_end - time_start).seconds
+    minute = time_diff // 60
+    if request.method == "POST":
+        user.credit -= 60 + 25*(minute // 25)
+        user_queueman.credit +=  int((60 + 25*(minute // 25)) / 2)
+        user.book = None
+        operation_user.delete()
+        user_queueman.save()
+        user.save()
+        return customer_complete(request)
+    return render(request,'customer_payment.html',{'operation':operation_user,'price':60 + 25*(minute // 25),'credit':user.credit})
+
+def customer_complete(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request,'customer_complete.html')
+
+    
+def customer_review(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request,'customer_review.html')
+
+def customer_report(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('login'))
+    return render(request,'customer_report.html')
