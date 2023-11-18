3663beb05bbb26d28d59eb11eb8d841abe9e4fab
Chalisa Thummeraj <6410685041@cn103>
2023-11-19 01:15:00 +0700

    error customer nav

--- QueuePid/customers/views.py
+++ QueuePid/customers/views.py
@@ -54,12 +54,14 @@ def search(request):
 def about(request): # render to html
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
-    return render(request,'customer_about.html')
+    user = User_info.objects.get(username = request.user)
+    return render(request,'customer_about.html',{'user':user})
 
 def wallet(request): # render to html
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('login'))
-    return render(request,'customer_wallet.html')
+    user = User_info.objects.get(username = request.user)
+    return render(request,'customer_wallet.html',{'user':user})
 
 def account(request): # render to html
     if not request.user.is_authenticated:
