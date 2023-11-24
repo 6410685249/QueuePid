356feabcfe888d4d3ccaa483201ef8e4f742b7bc
peekice <peekice123@gmail.com>
2023-11-25 02:38:08 +0700

    edit urls

--- QueuePid/queueman/urls.py
+++ QueuePid/queueman/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('profile/',views.profile,name='qprofile'),
     path('profile/edit',views.edit_profile,name='qedit'),
     path('profile/change-password',views.change_password,name='qpassword'),
+    path('get_queue',views.get_queue,name='get_queue'),
     path('status',views.status,name='qstatus'),
     path('cancel',views.cancel,name='qcancel')
 ]
