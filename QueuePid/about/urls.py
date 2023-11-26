fc5d416e2fea628b3a9ada82a531fd19509891b5
Peerapat Ngamgsanga <6410685223@cn331>
2023-11-26 20:29:41 +0700

    add phone number

--- QueuePid/about/urls.py
+++ QueuePid/about/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.queuepid,name='queuepid'),
     path('about/',views.about_queuepid,name='about_queuepid'),
     path('more/',views.more_about_us, name="more_about_us"),
+
 ]
--- QueuePid/queueman/models.py
+++ QueuePid/queueman/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from location_field.models.plain import PlainLocationField
+
 # Create your models here.
 class Queueman(models.Model):
     username = models.OneToOneField(User,on_delete=models.CASCADE)
@@ -9,6 +11,7 @@ class Queueman(models.Model):
     credit = models.PositiveIntegerField(default=0)
     is_have_queue = models.BooleanField(default=False)
     upload =  models.PositiveIntegerField(default=0)
+    location_address = PlainLocationField(zoom=7, null=True,default=None,blank=True)
     
     def __str__(self):
         return f'{self.username}'
